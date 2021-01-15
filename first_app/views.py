1246a7e19e0dbae2631a182f589580b6955cbe73
ChristinaGaitan <christina.gaitan@gmail.com>
2021-01-15 08:46:33 -0600

    Show access records in index view

--- first_app/views.py
+++ first_app/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from first_app.models import Topic, Webpage, AccessRecord
 
 # Create your views here.
 def index(request):
-  my_dictionary = { 'insert_content': 'hello I am from first app' }
-  return render(request, 'first_app/index.html', context=my_dictionary)
+  webpage_list = AccessRecord.objects.order_by('date')
+  date_dictionary = {'access_records': webpage_list}
+  return render(request, 'first_app/index.html', context=date_dictionary)
