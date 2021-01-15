ed0f625357a49963c85e10186fcd73e14b716ec2
ChristinaGaitan <christina.gaitan@gmail.com>
2021-01-15 09:23:10 -0600

    Create user model, view and template

--- first_app/models.py
+++ first_app/models.py
@@ -21,3 +21,8 @@ class AccessRecord(models.Model):
 
   def __str__(self):
     return str(self.date)
+
+class User(models.Model):
+  first_name = models.CharField(max_length=128)
+  last_name  = models.CharField(max_length=128)
+  email      = models.EmailField(max_length=128, unique=True)
--- first_app/views.py
+++ first_app/views.py
@@ -1,9 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from first_app.models import Topic, Webpage, AccessRecord
+from first_app.models import Topic, Webpage, AccessRecord, User
 
 # Create your views here.
 def index(request):
   webpage_list = AccessRecord.objects.order_by('date')
   date_dictionary = {'access_records': webpage_list}
   return render(request, 'first_app/index.html', context=date_dictionary)
+
+def users(request):
+  user_list = User.objects.order_by('first_name')
+  user_dictionary = { 'users': user_list }
+  return render(request, 'first_app/index.html', context=user_dictionary)
