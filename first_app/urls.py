2a9829db6a5659ef83cdd1087f8c33543c379c5c
ChristinaGaitan <christina.gaitan@gmail.com>
2021-01-20 09:19:58 -0600

    Create new view, url and template

--- first_app/urls.py
+++ first_app/urls.py
@@ -3,5 +3,6 @@ from first_app import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('new_user', views.new_user, name='new_user'),
     path('users', views.users, name='users'),
 ]
